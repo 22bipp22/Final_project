cdc28e9bb800d4726ab54cbeff7fc44d0e39dd5e
22bipp22 <tammibippert@gmail.com>
2020-12-14 20:20:03 -0600

    fix main-etl

--- static/js/plot.js
+++ static/js/plot.js
@@ -2,11 +2,11 @@ function initializeViz() {
     // JS object that points at empty div in the html
     let placeholderDiv = document.getElementById("tableauViz");
     // URL of the viz to be embedded
-    let url = "https://prod-useast-b.online.tableau.com/t/22bipp22/views/tableau_practice/Obesity?:showAppBanner=false&:display_count=n&:showVizHome=n&:origin=viz_share_link";
+    let url = "http://public.tableau.com/views/Top10StationsDashboard_16062498361870/Top10Stations";
     // An object that contains options specifying how to embed the viz
     let options = {
-      width: '600px',
-      height: '600px',
+      width: '1000px',
+      height: '1000px',
       hideTabs: true,
       hideToolbar: true,
     };
