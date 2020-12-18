b68f74cfac5a9dcf19e0314f9880a6953c7770d9
22bipp22 <tammibippert@gmail.com>
2020-12-17 18:36:02 -0600

    prettied it up

--- static/js/logic.js
+++ static/js/logic.js
@@ -21,18 +21,30 @@
       // Create the Table Header Row
       let webBody = d3.select("body");
       let table = webBody.append("table")
+      table.attr("style", "margin: 20px; width: 100%")
+         
+      
       let thead = table.append("thead")
       let trow = thead.append("tr")
       let header = trow.append("th")
       header.text("Horse Number")
+            .attr("style", "width: 20%; text-align: center")
+            
       let header2 = trow.append("th")
       header2.text("Race Distance")
+            .attr("style", "width: 20%; text-align: center")
+            
       let header3 = trow.append("th")
       header3.text("Place Odds")
+            .attr("style", "width: 20%; text-align: center")
+            
       let header4 = trow.append("th")
       header4.text("Result")
+            .attr("style", "width: 20%; text-align: center")
+           
       let header5 = trow.append("th")
       header5.text("Winner")
+            .attr("style", "width: 20%; text-align: center")
   
     //   // Populate the table data
       let tableBody = table.append("tbody")
@@ -45,29 +57,46 @@
             // if (number == raceData[i].horse_id) {
               let row = tableBody.append('tr');
               let cell = row.append('td');
-              cell.text(raceData[i].horse_id);
-              let cell2 = row.append('td')
-              cell2.text(raceData[i].race_id);
+              cell.text(raceData[i].horse_id)
+                  .attr("style", "text-align: center");
+              // let cell2 = row.append('td')
+              // cell2.text(raceData[i].race_id)
+              //     .attr("style", "text-align: center");
               let cell3 = row.append('td')
-              cell3.text(raceData[i].distance);
+              cell3.text(raceData[i].distance)
+                  .attr("style", "text-align: center");
               let cell4 = row.append('td')
-              cell4.text(raceData[i].place_odds);
+              cell4.text(raceData[i].place_odds)
+                  .attr("style", "text-align: center");
               let cell5 = row.append('td')
-              cell5.text(raceData[i].result);
+              cell5.text(raceData[i].race_id)
+                  .attr("style", "text-align: center");
               let cell6 = row.append('td')
               // Check to see if the horse won or lost the race and populate accordingly
               if (raceData[i].won == 0) { 
-                cell6.text("Lost");
+                cell6.text("Lost")
+                  .attr("style", "text-align: center");
               }
                 else {
-                  cell6.text("Won");
+                  cell6.text("Won")
+                      .attr("style", "text-align: center");
                 }
             }
           // }
-  
+          let svgWidth = window.innerWidth;
+          let svgHeight = "400px";
+    
+          let imagepath = "../static/data/Generic_track.jpg" 
+          webBody.append("img")
+                  .attr("width", svgWidth)
+                  .attr("height", svgHeight)
+                  .attr("src", imagepath)
+                  .attr("style", "padding: 50px")
     
           
       });  
+
+     
     
     // });
   
